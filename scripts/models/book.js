bcd2851f684ad852e58d19a457b2c7f25ec1451e
Josh West <joshw@zillowgroup.com>
2018-02-27 12:07:01 -0800

    added syntax to js. steps 1-7 on lab

---
+++ scripts/models/book.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var __API_URL__ = 'https://api-v1-books';
+
+// create a constructor function wrapped in IIFE
+(function (module) {
+  function Book (bookDataObj) {
+    Book.all = [];
+    this.book_id = book_id,
+    this.author = author,
+    this.title = title, 
+    this.isbn = isbn, 
+    this.image_url = image_url,
+    this.description = description
+  }
+
+  //book instance method
+  Book.prototype.toHtml = function() {
+    var template = Handlebars.compile($('#book-list-template').text());
+    return template(this);
+  };
+
+  //sorting rows by title, creating an array of book instances, assigning to Book.all array
+  Book.loadAll = rows => {
+    Book.all = rows.sort((a, b) => b.title - a.title).map(book => new Book(book));
+  }
+
+  Book.fetchAll = callback => {
+    $.get(`${__API_URL__}/api/v1/books`)
+    }
+  }
+  
+})(app)
+
