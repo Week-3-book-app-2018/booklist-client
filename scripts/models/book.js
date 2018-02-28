7dd811d5017159faa8751d949b7e667cbec667fd
Josh West <joshw@zillowgroup.com>
2018-02-28 10:11:49 -0800

    updating html

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -17,14 +17,6 @@ const __API_URL__ = 'https://localhost:3000';
     //Allie's Way
     //Object is build in to js, gives an array of the keys. We then map over them to get an object literal
     Object.keys(bookDataObj).forEach(key => this[key] = bookDataObj[key]);
-
-    //Our way. Maybe use other.
-    // this.book_id = book_id,
-    // this.author = author,
-    // this.title = title, 
-    // this.isbn = isbn, 
-    // this.image_url = image_url,
-    // this.description = description
   }
 
   //book instance method
